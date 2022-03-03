c4a19a7f8d04a6428c0d7c7f36bb36ee2ecc477e
unknown <jeuxdeau2009@gmail.com>
2022-03-03 16:03:15 -0500

    header , added tailwind forms

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import Header from '../components/Header';
+
+import styles from '../styles/Home.module.css'
+
+
+
+export default function Home() {
+  return (
+    <div className= "">
+      <Head>
+        <title> Instagram 2.0 from youtube</title>
+      </Head>
+
+      {/*Header   */}
+      <Header /> 
+
+
+      {/*Feed  */}
+
+      {/*Modal  */}
+
+
+    </div>
+  );
+} 
